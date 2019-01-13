fd64120076c5ffbc1a64df6e102a2817e224127e
saihn <saihn@uw.edu>
2019-01-12 16:47:07 -0800

    lior & harshita changes

--- index.js
+++ index.js
@@ -5,25 +5,6 @@ function signup() {
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
 
-    // firebase.auth().createUserWithEmailAndPassword(email, password)
-    //     .catch(function (error) {
-
-    //         // Handle Errors here.
-    //         var errorCode = error.code;
-    //         var errorMessage = error.message;
-    //         if (errorCode == 'auth/weak-password') {
-    //             alert('The password is too weak.');
-
-    //         } else if (errorCode == 'auth/email-already-in-use') {
-    //             alert('This email is already in use. Please select a different one.');
-
-    //         } else {
-    //             alert(errorMessage);
-
-    //         }
-    //         console.log(error);
-    //     }).then(window.location = "./createProfile.html");
-
     firebase.auth().createUserWithEmailAndPassword(email, password)
         .then(() => {
             glob_email = email;
@@ -51,6 +32,36 @@ function signup() {
 
 }
 
+function signin() {
+    let email = document.getElementById("emailSignIn").value;
+    let password = document.getElementById("passwordSignIn").value;
+
+    firebase.auth().signInWithEmailAndPassword(email, password).then(() =>{
+        console.log("signed in");
+    }).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        
+        if (errorCode == 'auth/user-not-found' || errorCode == 'auth/wrong-password') {
+            alert('Incorrect email or password. Please try again.');
+        } else {
+            alert(errorMessage);
+
+        }
+        console.log(error);
+    });
+}
+
+function signout() {
+    firebase.auth().signOut().then(function() {
+        // Sign-out successful.
+        console.log("signed out");
+      }).catch(function(error) {
+        // An error happened.
+      });
+}
+
 function createProfile() {
     let first = document.getElementById("firstName").value;
     let last = document.getElementById("lastName").value;
@@ -61,12 +72,41 @@ function createProfile() {
 
     if (first != "" && last != "" && birthday != "" && bio != "") {
         firebase.database().ref("users/" + user.uid).set({
+            uid: user.uid,
             first_name: first,
             last_name: last,
             birthday: birthday,
             bio: bio
+        }).then(() =>{
+            document.getElementById("createProfile").style.display = "none";
         });
-    }else{
+    } else {
         alert("Please fill out all inputs.");
     }
 }
+
+function updateTextInput(val) {
+    document.getElementById('textInput').value = val * 30;
+}
+
+function getNeedsInfo() {
+    let user = firebase.auth().currentUser;
+    var name = "";
+    firebase.database().ref("users/" + user.uid).on('value', function(snapshot) {
+        name = (snapshot.val() && (snapshot.val().first_name + " " + snapshot.val().last_name)) || 'Anonymous';
+    });
+    let address = document.getElementById("address");
+    let jobs = document.getElementsByClassName("job");
+
+    let jobList = [];
+
+    for(var i = 0; i < jobs.length; i++){
+        if (jobs[i].checked && jobs[i].name == "other"){
+            jobList.push(document.getElementById("other").value);
+        }else if(jobs[i].checked){
+            jobList.push(jobs[i].name);
+        }
+    }
+    console.log(jobList);
+}
+
